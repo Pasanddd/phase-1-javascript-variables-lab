9a46d78d0f24ec830f38ccc78b9828e74a109b23
pasanddd <pasandyaba@gmail.com>
2024-06-02 15:30:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+// Declare and initialize variables
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+// Export variables if needed (not mentioned in the lab)
+module.exports = {
+  companyName,
+  mostProfitableNeighborhood,
+  companyCeo
+};
